ec15bb10f6de46c47e03f3a7e0f41e104dca3e8e
Melody Arinze <arinze.melody2999@gmail.com>
2025-05-24 17:15:06 -0700

    Add scripts for repetition, square printing, and addition

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,8 +1,6 @@
-const arg = process.argv[2];            // Get the first argument from the command line
-const number = parseInt(arg);           // Convert it to an integer
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-if (!isNaN(number)) {                   // Check if the result is a valid number
-  console.log(`My number: ${number}`);  // If valid, print it
-} else {
-  console.log('Not a number');          // If invalid, print this
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
 }
+
